e7762c89e2b301730e5f78c0f18882c4fd313286
Dawid Gosławski <50369002+dg-nvm@users.noreply.github.com>
2020-10-27 14:52:25 +0100

    Support both HOME and USERPROFILE

--- clink_cmder_tilde.lua
+++ clink_cmder_tilde.lua
@@ -1,15 +1,17 @@
--- Add this file to <cmder_root>/config/tilde.lua
-
 local function tilde_match (text, f, l)
+    home = clink.get_env('home')
+    if not home then
+        home = clink.get_env('USERPROFILE')
+    end
     if text == '~' then
-        clink.add_match(clink.get_env('home'))
+        clink.add_match(home)
         clink.matches_are_files()
         return true
     end
     if text:sub(1, 1) == '~' then
-        clink.add_match(string.gsub(text, "~", clink.get_env('home'), 1))
+        clink.add_match(string.gsub(text, "~", home, 1))
         -- second match prevents adding a space so we can look for more matches
-        clink.add_match(string.gsub(text, "~", clink.get_env('home'), 1) .. '+')
+        clink.add_match(string.gsub(text, "~", home, 1) .. '+')
         clink.matches_are_files()
         return true
     end
